7ebb40cc935d80699dd7bf0437ac19d0345a8321
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2015-04-13 20:31:18 +0200

    Added a very primitive router.

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'KLF index.php';
+switch($_SERVER['REQUEST_URI']) {
+    case '/':
+        echo 'This is the home page';
+        break;
+    case '/about':
+        echo 'This is the about page';
+        break;
+    default:
+        echo sprintf('%s is not found on this server.', $_SERVER['REQUEST_URI']);
+}
